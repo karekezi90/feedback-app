831fcb81537836329ed63c1aa400fd0f239bba14
karekezi90 <28229829+karekezi90@users.noreply.github.com>
2021-12-28 20:28:22 +0800

    second commit

---
+++ src/components/shared/Spinner.jsx
@@ -0,0 +1,17 @@
+// import spinner from '../assets/spinner.gif'
+
+function Spinner() {
+    return (
+        <img 
+            src="./spinner.gif" 
+            alt="Loading..." 
+            style={{
+                width: '100px',
+                margin: 'auto',
+                display: 'block'
+            }}
+        />
+    )
+}
+
+export default Spinner
